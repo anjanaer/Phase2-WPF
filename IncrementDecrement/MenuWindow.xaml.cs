e03c44f5a6fc1f7b9a10605950a1f231c56ea45a
2021508 <2021508@TVMLT1211>
2024-09-27 17:37:55 +0530

    Add project files.

---
+++ IncrementDecrement/MenuWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace IncrementDecrement
+{
+    /// <summary>
+    /// Interaction logic for MenuWindow.xaml
+    /// </summary>
+    public partial class MenuWindow : Window
+    {
+        public MenuWindow()
+        {
+            InitializeComponent();
+        }
+        private MainWindow mainWindow = new MainWindow();
+        private SumCalcWindow sumCalcWindow = new SumCalcWindow();
+        private SquareCalc squareCalcWindow = new SquareCalc();
+      
+     
+
+        private void btnCounter_Click(object sender, RoutedEventArgs e)
+        {
+            mainWindow.Show();
+            mainWindow.Activate();
+        }
+
+        private void btnSumCalc_Click(object sender, RoutedEventArgs e)
+        {
+            sumCalcWindow.Show();
+            sumCalcWindow.Activate();
+        }
+
+        private void btnSquareCubeCalc_Click(object sender, RoutedEventArgs e)
+        {
+            squareCalcWindow.Show();
+            squareCalcWindow.Activate();
+        }
+    }
+}
---
+++ IncrementDecrement/SquareCalc.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace IncrementDecrement
+{
+    /// <summary>
+    /// Interaction logic for SquareCalc.xaml
+    /// </summary>
+    public partial class SquareCalc : Window
+    {
+        public SquareCalc()
+        {
+            InitializeComponent();
+        }
+        private bool IsNum(string numStr)
+        {
+            try
+            {
+                int.Parse(numStr);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+
+        }
+
+        private void btnSquare_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (!IsNum(txtNumber.Text))
+            {
+                return;
+            }
+            if (!IsNum(txtNumber.Text))
+            {
+                return;
+            }
+            int number1 = int.Parse(txtNumber.Text);
+            int square = number1 * number1;
+            txtResult.Text = square.ToString();
+            lblResult.Text = "Square";
+
+        }
+
+        private void btnCube_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (!IsNum(txtNumber.Text))
+            {
+                return;
+            }
+            if (!IsNum(txtNumber.Text))
+            {
+                return;
+            }
+            int number1 = int.Parse(txtNumber.Text);
+
+            int cube = number1*number1*number1;
+            txtResult.Text = cube.ToString();
+            lblResult.Text = "Cube";
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            e.Cancel = true;
+            this.Hide();
+        }
+    }
+}
