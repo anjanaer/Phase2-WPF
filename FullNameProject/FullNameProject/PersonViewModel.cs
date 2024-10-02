7e9c94559f236e04933bd975b9db2ee3625ae639
anjanaer <73066387+anjanaer@users.noreply.github.com>
2024-10-02 17:08:04 +0530

    Add files via upload

---
+++ FullNameProject/FullNameProject/PersonViewModel.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows;
+
+namespace FullNameProject
+{
+
+    public delegate void DWidnowClose();
+    public class PersonViewModel : INotifyPropertyChanged
+        {
+        
+            public DWidnowClose NewWindowClose;
+            public DWidnowClose EditWindowClose;
+            //
+            public event PropertyChangedEventHandler PropertyChanged;
+            private void OnPropertyChanged(string propName)
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+            }
+            //        
+            private PersonModel _person = null;
+            public PersonModel Person
+            {
+                get => _person;
+                set { _person = value; OnPropertyChanged(nameof(Person)); }
+            }
+            //
+            private PersonModel _editablePerson = null;
+            public PersonModel EditablePerson
+            {
+                get => _editablePerson;
+                set { _editablePerson = value; OnPropertyChanged(nameof(EditablePerson)); }
+            }
+
+            
+            //
+          
+            
+            public ICommand UpdateCommand { get; }
+           
+            public PersonViewModel()
+            {
+                this.Person = new PersonModel {FirstName="Anjana" ,LastName="E R"};
+                this.EditablePerson = new PersonModel();
+
+                UpdateCommand = new RelayCommand(Update);
+
+            }
+            
+            public void Update()
+            {
+                if (this.EditablePerson == null)
+                {
+                    return;
+                }
+                this.Person= this.EditablePerson;
+                var result = MessageBox.Show(messageBoxText: "Updated Successfully",
+                        caption: "Alert",
+                        button: MessageBoxButton.OK,
+                        icon: MessageBoxImage.Information);
+                if (EditWindowClose != null)
+                {
+                    EditWindowClose();
+                }
+            }
+            
+        }
+    }
+
